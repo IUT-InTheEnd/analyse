bb855b186ee6c96c17705be431fa52b16221f001
CyprienIUT <cpe.albert2@gmail.com>
2025-10-08 09:10:23 +0200

    clean raw genre script fix

--- clean_raw_genres.py
+++ clean_raw_genres.py
@@ -1,9 +1,9 @@
 import pandas as pd
-chemin_to_csv = "./Dataset/raw_genres.csv"
+chemin_to_csv = "./dataset/raw_genres.csv"
 
 df = pd.read_csv(chemin_to_csv)
 
 # Colonnes supprimées car genre color n'a aucun intéret et genre handle est redondant avec genre title
 df = df.drop(columns=["genre_color","genre_handle"])
 
-df.to_csv("./Cleaned_Dataset/clean_raw_genres.csv",index=False)
+df.to_csv("./cleaned_dataset/clean_raw_genres.csv",index=False)
