b3c92077cec2d8fa1067ce8d85c454d7e787c287
Matheo <babouin.panda@gmail.com>
2025-10-08 09:26:38 +0200

    clean_genres

--- clean_genres.py
+++ clean_genres.py
@@ -4,7 +4,7 @@ genres = pd.read_csv("./dataset/genres.csv")
 # rename des columns pour que ça soit plus clair en database
 genres = genres.rename(columns={'parent': 'genre_parent_id', 'title': 'genre_title'})
 
-# replace des valeurs de top_level par un bool 0 ou 1
+# replace des valeurs de top_level par un bool
 genres['top_level'] = (genres['genre_parent_id'] == 0)
 print(genres)
 
