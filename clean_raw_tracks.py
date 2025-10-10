d4aa47c9570aa9a4565f1880e1a4e760df01331f
CyprienIUT <cpe.albert2@gmail.com>
2025-10-10 15:22:26 +0200

    update clean raw tracks (attributs retenus)

--- clean_raw_tracks.py
+++ clean_raw_tracks.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
+# import from csv
 df = pd.read_csv("./dataset/raw_tracks.csv")
 
 # track date recorded : 103283 / 109727 qui sont NaN
-# les autres attributs ne sont pas intéressantes à traiter ou manque de données
+# Les attributs suivants ne sont pas intéressantes à traiter ou manque de données
 df = df.drop(columns=["album_title","album_url","artist_name","artist_url","artist_website",
                       "license_image_file","track_image_file","license_url","tags","track_bit_rate",
                       "track_comments","track_copyright_c","track_copyright_p","track_disc_number",
@@ -12,11 +13,26 @@ df = df.drop(columns=["album_title","album_url","artist_name","artist_url","arti
                       "track_date_recorded", "license_title","track_composer"
                 ])
 
+'''
+Attributs retenus :
+    - track_id : identifiant unique de la musique
+    - album_id : identifiant unique de l'album
+    - artist_id : identifiant unique de l'artiste
+    - license_parent_id : identifiant de la licence
+    - track_date_created : date d'ajout du morceau sur la plateforme
+    - track_duration : durée du morceau
+    - track_explicit : indique si le morceau est explicite ou non (valeurs possibles : "Radio-Safe", "Radio-Unsafe","Adults-Only")
+    - track_favorites : nombre de favoris sur le morceau
+    - track_genres : genres associés au morceau
+    - track_instrumental : indique si le morceau est instrumental ou non (valeurs possibles : 0 ou 1)
+    - track_interest : score d'intérêt du morceau (FMA)
+    - track_listens : nombre d'écoutes du morceau
+    - track_title : titre du morceau
+'''
+
 # track_explicit treatment (NaN -> Radio-Safe)
 df_temp = df["track_explicit"].fillna("Radio-Safe")
 df["track_explicit"] = df_temp
 
+# export to csv
 df.to_csv("./cleaned_dataset/clean_raw_tracks.csv",index=False)
-
-
-
